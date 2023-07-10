4db30d2faab419c5120ad04438a3180927ac92c0
smrTofighi <mrtofxn@gmail.com>
2023-07-10 09:54:38 -0400

    create light app class

---
+++ lib/res/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LightAppColor {
+  LightAppColor._();
+  static const Color primary = Colors.blue;
+
+  static const Color title = Colors.black;
+  static const Color hint = Color.fromARGB(255, 217, 220, 228);
+  static const Color appBar = Colors.white;
+  static const Color focusedTextField = Colors.white;
+  static const Color unfocusedTextField = Color.fromARGB(255, 251, 251, 251);
+  static const Color border = Color.fromARGB(255, 217, 220, 228);
+  static const Color focusedBorder = Color.fromARGB(255, 0, 117, 251);
+  static const Color button = Color.fromARGB(255, 0, 117, 251);
+  static const Color suraface = Color.fromARGB(255, 243, 243, 243);
+  static const Color scaffoldBG = Color.fromARGB(255, 251, 251, 251);
+}
